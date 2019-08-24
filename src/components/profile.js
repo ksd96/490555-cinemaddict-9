b418d1a5bcbf9d985a700c83e4c95aae28c7df1b
ksd96 <45177616+ksd96@users.noreply.github.com>
2019-08-24 17:00:45 +0300

    исправил ошибки

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,10 +1,5 @@
 import {createElement} from './utils.js';
 
-export const makeElementProfile = ({rank}) => `<section class="header__profile profile">
-    <p class="profile__rating">${rank}</p>
-    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-  </section>`.trim();
-
 export class Profile {
   constructor(rank) {
     this._rank = rank;
--- src/main.js
+++ src/main.js
@@ -11,12 +11,12 @@ import {arrayComents} from './components/data-comments.js';
 import {Comments} from './components/film-details-comments.js';
 import {arrayFilms} from './components/array-films.js';
 import {totalCard} from './components/array-films.js';
-import {Position, render, unrender} from './components/utils.js';
+import {render, unrender} from './components/utils.js';
 
 
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
-const sectionFilms = document.createElement(`section`);
+const sectionFilms = document.createElement(`section`).classList.add(`films`);
 const sectionFilmsList = document.createElement(`section`);
 const filmListTitle = `<h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>`;
 const filmListContainer = document.createElement(`div`);
@@ -34,38 +34,53 @@ const addComponent = (where, what) => {
   where.insertAdjacentHTML(`beforeend`, what);
 };
 
-const makeLoadMore = () => {
-  const number = 5;
+const renderShowMore = () => {
+  const makeLoadMore = () => {
+    const number = 5;
+    const pageCount = filmListContainer.querySelectorAll(`.film-card`).length;
 
-  arrayFilms.slice(renderStart + number, renderEnd + number).forEach((filmMock) => renderFilmCard(filmMock, filmListContainer));
-  renderStart += number;
-  renderEnd += number;
-  if (renderEnd >= totalCard) {
-    load.classList.add(`visually-hidden`);
-  }
+    arrayFilms.slice(pageCount, pageCount + number).forEach((filmMock) => renderFilmCard(filmMock, filmListContainer));
+    if (pageCount >= totalCard) {
+      load.classList.add(`visually-hidden`);
+    }
+  };
+
+  addComponent(sectionFilmsList, getElementShowMore());
+  const load = document.querySelector(`.films-list__show-more`);
+  load.addEventListener(`click`, makeLoadMore);
 };
 
 const renderProfile = (profileMock) => {
   const profile = new Profile(profileMock);
-  render(header, profile.getElement(), Position.BEFOREEND);
+  render(header, profile.getElement());
 };
 
 const renderNavigation = (navMock) => {
   const nav = new Navigation(navMock);
-  render(navigation, nav.getElement(), Position.BEFOREEND);
-};
+  if (!main.querySelector(`.main-navigation`)) {
+    navigation.classList.add(`main-navigation`);
+    main.appendChild(navigation);
+  }
 
-let commentsList = null;
-const renderComment = (commentMock) => {
-  const comment = new Comments(commentMock);
-  commentsList = document.querySelector(`.film-details__comments-list`);
-  render(commentsList, comment.getElement(), Position.BEFOREEND);
+  render(navigation, nav.getElement());
 };
-
 
 const renderFilmCard = (filmMock, where) => {
   const film = new Film(filmMock);
   const filmDetails = new FilmDetails(filmMock);
+
+  if (!sectionFilmsList.querySelector(`.films-list__container`)) {
+    filmListContainer.classList.add(`films-list__container`);
+    sectionFilmsList.appendChild(filmListContainer);
+  }
+
+  let commentsList = null;
+
+  const renderComment = (commentMock) => {
+    const comment = new Comments(commentMock);
+    commentsList = document.querySelector(`.film-details__comments-list`);
+    render(commentsList, comment.getElement());
+  };
 
   const onEscKeyDown = (evt) => {
     if (evt.key === `Escape` || evt.key === `Esc`) {
@@ -78,7 +93,7 @@ const renderFilmCard = (filmMock, where) => {
   film.getElement()
     .querySelector(`img`)
     .addEventListener(`click`, () => {
-      render(body, filmDetails.getElement(), Position.BEFOREEND);
+      render(body, filmDetails.getElement());
       arrayComents.forEach((commentMock) => renderComment(commentMock));
       document.addEventListener(`keydown`, onEscKeyDown);
     });
@@ -86,7 +101,7 @@ const renderFilmCard = (filmMock, where) => {
   film.getElement()
     .querySelector(`.film-card__title`)
     .addEventListener(`click`, () => {
-      render(body, filmDetails.getElement(), Position.BEFOREEND);
+      render(body, filmDetails.getElement());
       arrayComents.forEach((commentMock) => renderComment(commentMock));
       document.addEventListener(`keydown`, onEscKeyDown);
     });
@@ -94,8 +109,18 @@ const renderFilmCard = (filmMock, where) => {
   film.getElement()
     .querySelector(`.film-card__comments`)
     .addEventListener(`click`, () => {
-      render(body, filmDetails.getElement(), Position.BEFOREEND);
+      render(body, filmDetails.getElement());
       arrayComents.forEach((commentMock) => renderComment(commentMock));
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+  filmDetails.getElement().querySelector(`textarea`)
+    .addEventListener(`focus`, () => {
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+  filmDetails.getElement().querySelector(`textarea`)
+    .addEventListener(`blur`, () => {
       document.addEventListener(`keydown`, onEscKeyDown);
     });
 
@@ -107,15 +132,28 @@ const renderFilmCard = (filmMock, where) => {
       document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
-  render(where, film.getElement(), Position.BEFOREEND);
+  render(where, film.getElement());
+};
+
+const renderFilmsExtra = (section, container, title, first, second) => {
+  section.classList.add(`films-list--extra`);
+  sectionFilms.appendChild(section);
+
+  addComponent(section, title);
+
+  container.classList.add(`films-list__container`);
+  section.appendChild(container);
+
+  arrayFilms.slice(first, second).forEach((filmMock) => renderFilmCard(filmMock, container));
 };
 
 addComponent(header, getElementSearch());
-navigation.classList.add(`main-navigation`);
-main.appendChild(navigation);
+
+arrayNavigation.forEach((navMock) => renderNavigation(navMock));
+
 addComponent(main, getElementSort());
 
-sectionFilms.classList.add(`films`);
+//sectionFilms.classList.add(`films`);
 main.appendChild(sectionFilms);
 
 sectionFilmsList.classList.add(`films-list`);
@@ -123,36 +161,13 @@ sectionFilms.appendChild(sectionFilmsList);
 
 addComponent(sectionFilmsList, filmListTitle);
 
-filmListContainer.classList.add(`films-list__container`);
-sectionFilmsList.appendChild(filmListContainer);
 arrayFilms.slice(0, 5).forEach((filmMock) => renderFilmCard(filmMock, filmListContainer));
-arrayFilms.slice(0, 2).forEach((filmMock) => renderFilmCard(filmMock, filmListContainerTopRated));
-arrayFilms.slice(3, 5).forEach((filmMock) => renderFilmCard(filmMock, filmListContainerMostCommented));
-arrayNavigation.forEach((navMock) => renderNavigation(navMock));
+
+renderFilmsExtra(sectionFilmListTopRated, filmListContainerTopRated, filmListTopRatedTitle, 0, 2);
+renderFilmsExtra(sectionFilmListMostCommented, filmListContainerMostCommented, filmListMostCommentedTitle, 3, 5);
+
 renderProfile(numberWatched);
 
-addComponent(sectionFilmsList, getElementShowMore());
-
-sectionFilmListTopRated.classList.add(`films-list--extra`);
-sectionFilms.appendChild(sectionFilmListTopRated);
-
-addComponent(sectionFilmListTopRated, filmListTopRatedTitle);
-
-filmListContainerTopRated.classList.add(`films-list__container`);
-sectionFilmListTopRated.appendChild(filmListContainerTopRated);
-
-
-sectionFilmListMostCommented.classList.add(`films-list--extra`);
-sectionFilms.appendChild(sectionFilmListMostCommented);
-
-addComponent(sectionFilmListMostCommented, filmListMostCommentedTitle);
-
-filmListContainerMostCommented.classList.add(`films-list__container`);
-sectionFilmListMostCommented.appendChild(filmListContainerMostCommented);
-
-const load = document.querySelector(`.films-list__show-more`);
-let renderStart = 0;
-let renderEnd = 5;
-load.addEventListener(`click`, makeLoadMore);
+renderShowMore();
 
 footerCount.innerHTML = `<p>${arrayFilms.length} movies inside</p>`;
